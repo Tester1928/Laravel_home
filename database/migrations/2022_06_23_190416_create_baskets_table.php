ff76acf98a5dea1dec893fc662b2fcbb121d41fc
Yura Rozzhalovets <y.rozzhalovets@brainforce.pro>
2022-06-23 22:11:57 +0300

    create migrate

---
+++ database/migrations/2022_06_23_190416_create_baskets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->id();
+            $table->text("products_id");
+            $table->unsignedFloat("price");
+            $table->unsignedInteger("user_id");
+            $table->text("user_comment")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baskets');
+    }
+}
